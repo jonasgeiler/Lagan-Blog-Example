8de0832447be106ec2f19a0a58e9d5f967e552e7
Skayo <jonasg.cool@gmail.com>
2018-10-12 00:23:49 +0200

    Added default config.php

---
+++ config.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file contains the settings for your database, error reporting
+ * and different paths and URL to your Lagan app.
+ * Add your database and path info and rename it to config.php for your app to work.
+ */
+
+/**
+ * @var string[] An array with your MySQL database configuration.
+ */
+$db_config = array(
+	'servername'	=> 'localhost',
+	'username'		=> 'root',
+	'password'		=> '',
+	'database'		=> 'lagan_blog'
+);
+
+/**
+ * @var string[] An array with admin usernames (key) and their passwords (value).
+ */
+$users = array(
+	'admin'	=> 'password'
+);
+
+/**
+ * @var string[] An array of the paths protected by the user password combination.
+ */
+$protected = array(
+	'/admin'
+);
+
+/**
+ * @const ERROR_REPORTING Enable or disable error reporting.
+ */
+define('ERROR_REPORTING', true);
+
+/**
+ * @const ROOT_PATH The server path to the root directory of your Lagan app (Should not have a trailing slash).
+ */
+define('ROOT_PATH', 'C:\path\to\Lagan-Blog-Example');
+/**
+ * @const APP_PATH The server path to the public directory of your Lagan app (Should not have a trailing slash).
+ */
+define('APP_PATH', 'C:\path\to\Lagan-Blog-Example\public');
+/**
+ * @const APP_URL The URL of your Lagan app (Should not have a trailing slash).
+ */
+define('APP_URL', 'http://localhost/Lagan-Blog/public/');
+?>
